a68b05e6da1734169eeaea05ab6aee9f8184696c
Andri Hendrawan <68363285+andrihendrawan0606@users.noreply.github.com>
2021-03-23 15:26:22 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$host ="localhost";
+$user ="root";
+$password ="";
+$database ="uprak1";
+$connect = mysqli_connect ($host,$user,$password,$database) or die("Gagal Menghubungkan"); //mengecek apakah sudah terkoneksi dengan database
+?>
